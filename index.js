079075dfc9fb7f7648c5ceab6e3c37b9d6414b24
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-10-17 12:06:47 -0400

    updated lab

--- index.js
+++ index.js
@@ -17,26 +17,43 @@ function addToList(shoe) {
     newShoe.addEventListener('click', () => {
         addToDisplay(shoe);
     })
+
+    newShoe.addEventListener('mouseover', () => {
+        newShoe.style.filter = 'drop-shadow(0 0 1em #ff4a6d)';
+        newShoe.style.border = '2px solid #ffecb8'
+    })
+
+    newShoe.addEventListener('mouseleave', () => {
+        newShoe.style.filter = '';
+        newShoe.style.border = '2px solid #ff4a6d'
+    })
+
 }
 
 function addToDisplay(shoe) {
     const displayImage = document.getElementById('detail-image');
     const displayName = document.getElementById('name');
-    const displayBrand = document.getElementById('brand');
-    const displayYear = document.getElementById('year-released');
-    const displayColorway = document.getElementById('og-colorway');
-    const displayDirector = document.getElementById('creative-director');
-    const displayRetail = document.getElementById('retail');
-    const displayResale = document.getElementById('resale');
+    const displayBrand = document.getElementById('brand-input');
+    const displayYear = document.getElementById('year-released-input');
+    const displayColorway = document.getElementById('og-colorway-input');
+    const displayDirector = document.getElementById('creative-director-input');
+    const displayRetail = document.getElementById('retail-input');
+    const displayResale = document.getElementById('resale-input');
     const displayDescrip = document.getElementById('description');
+
+    // const displayBrandContent = document.createElement('h3');
+    // displayBrandContent.className = 'name-content';
+    // displayBrandContent.textContent = shoe.brand;
+    // displayBrand.appendChild(displayBrandContent);
+
 
     displayImage.src = shoe.display_image;
     displayName.textContent = shoe.name;
-    displayBrand.textContent = `Brand: ${shoe.brand}`;
-    displayYear.textContent = `Released: ${shoe.release_year}`;
-    displayColorway.textContent = `Original Colorway: ${shoe.og_colorway}`;
-    displayDirector.textContent = `Creative Director: ${shoe.creative_director}`;
-    displayRetail.textContent = `Original Price: $${shoe.retail_price}`;
-    displayResale.textContent = `Current Value: $${shoe.resell_price}`;
+    displayBrand.textContent = shoe.brand;
+    displayYear.textContent = shoe.release_year;
+    displayColorway.textContent = shoe.og_colorway;
+    displayDirector.textContent = shoe.creative_director;
+    displayRetail.textContent = `$${shoe.retail_price}`;
+    displayResale.textContent = `$${shoe.resell_price}`;
     displayDescrip.textContent = shoe.description;
 }
