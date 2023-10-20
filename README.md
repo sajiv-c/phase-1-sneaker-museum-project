c10de562bad6e94581a393199dcf2b0027eab6d9
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-10-20 00:44:47 -0400

    completed project

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Phase 1 Project
+SNEAKER MUSEUM:
+
+    This is a simple web application that allows users to browse through and interact with a collection of sneakers. The application is built using JavaScript and makes use of the Fetch API to retrieve sneaker data from a local server running at http://localhost:3000/sneakers.
+
+Before running this web application, ensure that you have the following installed:
+
+    A web browser (e.g., Chrome, Firefox) to access the web application
+
+    A local server or a development environment to host the web application
+
+Usage:
+
+    Clone the repository or download the source code.
+
+    Ensure that you have a local server up and running and simply open the index.html file in your browser.
+
+    The website focuses on three popular brands: Nike, Air Jordan, and Adidas. Each brand offers four culturally significant sneakers, all well known to the market.
+
+    You should see a menu of sneakers under the logo. We’ve incorporated multiple EventListeners to each image, so feel free to click on any sneaker to view its details right below the menu.
+    
+    Each sneaker's details include release year, original color way, creative director, and more.
+
+    You can also add and delete comments under each sneaker by using the comment form at the bottom of the page.
--- index.js
+++ index.js
@@ -1,74 +1,78 @@
+// ---- declaring background gif URLs ----
 const nikeBackground = 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnBzYmRpYXlpcDAxMTFzOXN3cmtzMnM3dWt4MWQ5czRubHM5ODZkOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/9LZSXRm9wmkgWEiA5K/giphy.gif';
 const jordanBackground = 'https://media0.giphy.com/media/PQR24BJHzd5UiU1tjJ/giphy.gif?cid=ecf05e47cdniixyyp1zpx95mm1nr4u8qf9pa7lo6c5bohp8s&ep=v1_gifs_related&rid=giphy.gif&ct=s';
 const adidasBackground = 'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWVyd2FoMjE3ODFnbmtsY29qamxxNjZyYXN3dW93OHJ4MGY3N2VvayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/dUfFgEhDnVx6PFpsje/giphy.gif';
 
-let sneakers = null;
+// let sneakers = null;
+let sneaker = null;
+// let sneakerComments = null;
 
+// ---- initializing pointer towards 'details' element ----
 const detailsDiv = document.getElementById('details');
 
+// ---- GET request to populate sneaker menu and sneaker display, add default comments, and set default background gif ----
 fetch("http://localhost:3000/sneakers")
 .then(res => res.json())
 .then(shoes => {
-    sneakers = shoes;
+    // // sneakers = shoes;
     shoes.forEach(shoe => {
         addToList(shoe);
     })
-    document.querySelector('.comment-1 p').textContent = shoes[0].comments[0];
-    document.querySelector('.comment-2 p').textContent = shoes[0].comments[1];
-    // detailsDiv.style.backgroundImage = `url(${jordanBackground})`;
-    // detailsDiv.style.backgroundSize = '20%';
-    // detailsDiv.style.backgroundBlendMode = 'overlay';
+    document.querySelector('.comment-1 p').textContent = shoes[0].comments[0]
+    document.querySelector('.comment-2 p').textContent = shoes[0].comments[1]
     addToDisplay(shoes[0]);
     setBackgroundStyling(shoes[0])
 })
 
+// ---- function to set background gif depending on sneaker brand ----
 function setBackgroundStyling(shoe) {
     if (shoe.brand == 'Nike') {
         detailsDiv.style.backgroundImage = `url(${nikeBackground})`;
-        detailsDiv.style.backgroundSize = '20%'
-        detailsDiv.style.backgroundColor = 'rgb(43,43,43)'
-        detailsDiv.style.backgroundBlendMode = 'overlay'
-
+        detailsDiv.style.backgroundSize = '20%';
+        detailsDiv.style.backgroundColor = 'rgb(43,43,43)';
+        detailsDiv.style.backgroundBlendMode = 'overlay';
     }
     if (shoe.brand == 'Air Jordan') {
         detailsDiv.style.backgroundImage = `url(${jordanBackground})`;
-        detailsDiv.style.backgroundSize = '34%'
-        detailsDiv.style.backgroundColor = 'rgb(43,43,43)'
-        detailsDiv.style.backgroundBlendMode = 'overlay'
+        detailsDiv.style.backgroundSize = '34%';
+        detailsDiv.style.backgroundColor = 'rgb(43,43,43)';
+        detailsDiv.style.backgroundBlendMode = 'overlay';
+        detailsDiv.style.backgroundRepeat = 'round';
     }
     if (shoe.brand == 'Adidas') {
         detailsDiv.style.backgroundImage = `url(${adidasBackground})`;
-        detailsDiv.style.backgroundSize = '20%'
-        detailsDiv.style.backgroundColor = 'rgb(43,43,43)'
-        detailsDiv.style.backgroundBlendMode = 'multiply'
+        detailsDiv.style.backgroundSize = '20%';
+        detailsDiv.style.backgroundColor = 'rgb(43,43,43)';
+        detailsDiv.style.backgroundBlendMode = 'multiply';
+        detailsDiv.style.backgroundRepeat = 'round';
     }
 }
 
+// ---- function to add sneaker images to menu ----
 function addToList(shoe) {
     const newShoe = document.createElement('img');
     newShoe.src = shoe.menu_image;
     const shoeMenu = document.getElementById('sneaker-list');
     shoeMenu.appendChild(newShoe);
-    
-    // const displayCategories = document.getElementById('details');
 
-
+    // ---- adds sneaker details to display, sets default comments, and sets background gif on image click ----
     newShoe.addEventListener('click', () => {
         addToDisplay(shoe);
 
         document.querySelector('.comment-1 p').textContent = shoe.comments[0];
         document.querySelector('.comment-2 p').textContent = shoe.comments[1];
 
-        // Set background styling on click
-        setBackgroundStyling(shoe)
+        setBackgroundStyling(shoe);
     })
 
+    // ---- changes border, highlight, and image source on image mouseover ----
     newShoe.addEventListener('mouseover', () => {
         newShoe.style.filter = 'drop-shadow(0 0 1em #ff4a6d)';
         newShoe.style.border = '2px solid #ffecb8'
         newShoe.src = shoe.menu_gif;
     })
 
+    // ---- resets border, highlight, and image source on image mouseleave ----
     newShoe.addEventListener('mouseleave', () => {
         newShoe.style.filter = '';
         newShoe.style.border = '2px solid #ff4a6d'
@@ -77,6 +81,7 @@ function addToList(shoe) {
 
 }
 
+// ---- function to grab and populate all display elements ----
 function addToDisplay(shoe) {
     const displayImage = document.getElementById('detail-image');
     const displayName = document.getElementById('name');
@@ -97,13 +102,35 @@ function addToDisplay(shoe) {
     displayRetail.textContent = `$${shoe.retail_price}`;
     displayResale.textContent = `$${shoe.resell_price}`;
     displayDescrip.textContent = shoe.description;
+    // ---- sets 'sneaker' value to current sneaker ----
+    sneaker = shoe;
+    // sneakerComments = shoe.comments;
 }
 
+// ---- initializing the required pointers for commentForm's 'submit' EventListener ----
 const commentForm = document.getElementById('comment-form');
 const commentInput = document.getElementById('user-comment');
 const commentElements = document.getElementById('comment-elements')
+
+// ---- callback function to be used by 'delete' button EventListeners ----
+const handleDeleteComment = (deleteBtn) => {
+    const parentDiv = deleteBtn.parentNode;
+
+    parentDiv.remove();
+
+    // // ---- reassigns each delete button's index value ----
+    // const deleteBtns = document.querySelectorAll('.delete-comment');
+    //
+    // deleteBtns.forEach((btn, index) => {
+    //     btn.setAttribute("index", index)
+    // })
+    // 
+    // fetch(`http://localhost:3000/sneakers/${}`)
+}
+
+// adds EventListener to comments form to create new comment div and add to comments display ----
 commentForm.addEventListener('submit', (e) => {
-    
+    // ---- preventing reload on click ----
     e.preventDefault()
 
     const newCommentDiv = document.createElement('div');
@@ -115,49 +142,43 @@ commentForm.addEventListener('submit', (e) => {
     const deleteBtn = document.createElement('button');
     deleteBtn.className = 'delete-comment';
     deleteBtn.textContent = 'Delete';
+    // ---- adds delete EventListener to newly created 'delete' buttons ----
+    deleteBtn.addEventListener('click', () => handleDeleteComment(deleteBtn))
 
     newCommentDiv.appendChild(newCommentP);
     newCommentDiv.appendChild(deleteBtn);
 
     commentElements.appendChild(newCommentDiv);
 
+    // // ---- gives each delete button a targetable index value after adding comment ----
+    // const deleteBtns = document.querySelectorAll('.delete-comment');
+    // deleteBtns.forEach((btn, index) => {
+    //     btn.setAttribute("index", index)
+    // })
+    // // ---- PATCH request to add new comment to JSON server object ----
+    // fetch(`http://localhost:3000/sneakers/${sneaker.id}`, {
+    //     method: "PATCH",
+    //     headers: {"Content-Type": "application/json"},
+    //     body: JSON.stringify({
+    //         comments: [
+    //             ...sneaker.comments,
+    //             commentInput.value
+    //         ]
+    //     })
+    // })
+    // .then(res => res.json())
+    // .then(updatedSneaker => {
+    //     sneaker.comments = updatedSneaker.comments
+    // })
+
+    // ---- resetting comment input element ----
     commentInput.value = "";
-
-    const deleteBtns = document.querySelectorAll('.delete-comment');
-    console.log(deleteBtns);
-    deleteBtns.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            e.preventDefault()
-    
-            const parentDiv = btn.parentNode;
-    
-            console.log(parentDiv);
-    
-            parentDiv.remove();
-        })
-    })
 })
 
+// ---- adds delete EventListener to existing 'delete' buttons ----
 const deleteBtns = document.querySelectorAll('.delete-comment');
-    console.log(deleteBtns);
-    deleteBtns.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            e.preventDefault()
-    
-            const parentDiv = btn.parentNode;
-    
-            console.log(parentDiv);
-    
-            parentDiv.remove();
-    })
+deleteBtns.forEach(btn => {
+    btn.addEventListener('click', () => handleDeleteComment(btn));
 })
 
-// const dropdown = document.getElementById('dropdown');
-// const sneakerMenu = document.getElementById('sneaker-list');
-// dropdown.addEventListener('change', (e) => {
-//     if (e.target.value == 'nike') {
-//         sneakerMenu.appendChild(sneakers[0].menu_image);
-//         sneakerMenu.appendChild(sneakers[1])
-//     }
-// })
-
+// ---- commented code found above and in handleDeleteComment() was meant to add PATCH and DELETE requests to update comments in backend ----
